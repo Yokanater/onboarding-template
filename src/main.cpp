0a92ac80240160978e2af480b1c12c6fb1522f19
Yash Tripathi <yashtri22@gmail.com>
2026-08-28 23:57:34 +0530

    setup bare bones version

--- src/main.cpp
+++
@@ -1,6 +0,0 @@
-#include "submission.hpp"
-
-int main () {
-    
-    return 0;
-}
--- src/submission.hpp
+++ src/submission.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cstddef>
+#include <vector>
 
 // Starter Grid for the 2D heat-diffusion problem.
 //
@@ -11,14 +12,47 @@ class Grid {
 private:
   std::size_t rows_;
   std::size_t cols_;
+  std::vector<double> values_;
 
 public:
-  Grid(std::size_t rows, std::size_t cols);
+  Grid(std::size_t rows, std::size_t cols) : 
+    rows_{rows},
+    cols_{cols}, 
+    values_(rows*cols) 
+  {
+  }
 
-  double& operator()(std::size_t i, std::size_t j);
-  double  operator()(std::size_t i, std::size_t j) const;
+  double& operator()(std::size_t i, std::size_t j) {
+    return values_[cols_ * i + j];
+  }
+
+  double operator()(std::size_t i, std::size_t j) const {
+    return values_[cols_ * i + j];
+  }
+
+  std::size_t get_rows() const {
+    return rows_;
+  }
+  
+  std::size_t get_cols() const {
+    return cols_;
+  }
 };  
 
 // Apply the five-point stencil over all interior points, copying the boundary
 // values unchanged from old_grid to new_grid. Implement your solution here.
-void apply_stencil(const Grid& old_grid, Grid& new_grid);
+void apply_stencil(const Grid& old_grid, Grid& new_grid) {
+  const std::size_t rows = old_grid.get_rows();
+  const std::size_t cols = old_grid.get_cols();
+
+  for (std::size_t i = 0; i < rows; i++) {
+    for (std::size_t j = 0; j < cols; j++) {
+      if (i != 0 && i != rows - 1 && j != 0 && j != cols - 1) {
+        new_grid(i, j) = 0.5 * old_grid(i, j) + 0.125 * (old_grid(i-1, j) + old_grid(i+1, j) + old_grid(i, j-1) + old_grid(i, j+1));
+      }
+      else {
+        new_grid(i, j) = old_grid(i, j);
+      }
+    }
+  }
+}
